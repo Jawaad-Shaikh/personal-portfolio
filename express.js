d18ed3774a98bc60a89f0487d1c3f54f7e6c2642
Jawaad-Shaikh <jawaadshaikh1234@gmail.com>
2026-03-17 19:56:29 +0530

    Update express.js

--- express.js
+++ express.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Home route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'portfolio.html'));
 });
 
 app.listen(PORT, () => {
